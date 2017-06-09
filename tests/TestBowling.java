24364f0e26e87abbffddb68732260ba2b86ca72f
somename <someemail>
2017-06-09 19:59:15 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 public class TestBowling {
 
-	@Test
+	/*@Test
 	public void test() {
 		fail("Not yet implemented");
+	}*/
+	
+	@Test
+	public void test_Score(){
+		Frame frame = new Frame(3,5);
+		assertEquals(8,frame.score());
 	}
 
 }
